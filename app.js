5c140d107173bd33c6b013effc774778945f1dd3
aritra <aritrachowdhury65@gmail.com>
2022-08-31 20:43:56 +0530

    Privacy

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 var dob = document.querySelector(".dob");
-  var number = document.querySelector(".number");
-  var check = document.querySelector(".check");
-  var clear = document.querySelector(".clear");
-  var error = document.querySelector(".error");
-  var output = document.querySelector(".output");
+var number = document.querySelector(".number");
+var check = document.querySelector(".check");
+var clear = document.querySelector(".clear");
+var error = document.querySelector(".error");
+var output = document.querySelector(".output");
 
-  check.addEventListener("click", checkEventHandler);
-  clear.addEventListener("click", clearEventHandler);
+check.addEventListener("click", checkEventHandler);
+clear.addEventListener("click", clearEventHandler);
 
-  error.style.display = "none";
+error.style.display = "none";
 
-  function checkEventHandler(){
-    var birthday = (dob.value);
-    var remove = birthday.replaceAll("-","");
-    var sum=0
-    for(var i=0;i<remove.length;i++){
-      sum += Number(remove.charAt(i))
-    }
-   luckyCheck(sum);
+function checkEventHandler() {
+  var birthday = dob.value;
+  var remove = birthday.replaceAll("-", "");
+  var sum = 0;
+  for (var i = 0; i < remove.length; i++) {
+    sum += Number(remove.charAt(i));
   }
-
-  function luckyCheck(sum){
-  if(number.value !=0 && dob.value != "" )
-   {
-    output.style.display = "block"
-    error.style.display = "none";
-    if (sum % Number(number.value) == 0){
-      output.innerText = "Bingo, Your Birthday is Lucky 🎉🎉";
-    }else{
-      output.innerText = "Your Birthday is not that Lucky 😥😥";
-    }
-
-   }else {
-      output.style.display = "none";
-      error.style.display = "block";
-      error.innerText = "please enter both Date of Birth and Lucky number";
-   }
+  luckyCheck(sum);
 }
 
-  function clearEventHandler(){
-    window.location.reload();
-    console.log("clear");
+function luckyCheck(sum) {
+  if (number.value != 0 && dob.value != "") {
+    output.style.display = "block";
+    error.style.display = "none";
+    if (sum % Number(number.value) == 0) {
+      output.innerText = "Bingo, Your Birthday is Lucky 🎉🎉";
+    } else {
+      output.innerText = "Your Birthday is not that Lucky 😥😥";
+    }
+  } else {
+    output.style.display = "none";
+    error.style.display = "block";
+    error.innerText = "please enter both Date of Birth and Lucky number";
   }
+}
+
+function clearEventHandler() {
+  dob.value = "";
+  number.value = "";
+  error.style.display = "none";
+  output.style.display = "none";
+}
